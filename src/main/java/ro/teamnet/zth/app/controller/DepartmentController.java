19856fa50ee5b41ae8ae5bac68d7110a77a35dd1
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-05-07 11:35:45 +0300

    Modify getAllDepartments to return a list of departments

--- src/main/java/ro/teamnet/zth/app/controller/DepartmentController.java
+++ src/main/java/ro/teamnet/zth/app/controller/DepartmentController.java
@@ -2,6 +2,10 @@ package ro.teamnet.zth.app.controller;
 
 import ro.teamnet.zth.api.annotations.MyController;
 import ro.teamnet.zth.api.annotations.MyRequestMethod;
+import ro.teamnet.zth.app.domain.Department;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Author: Ovidiu
@@ -11,8 +15,21 @@ import ro.teamnet.zth.api.annotations.MyRequestMethod;
 public class DepartmentController {
 
     @MyRequestMethod(urlPath = "/all")
-    public String getAllDepartments() {
-        return "allDepartments";
+    public List<Department> getAllDepartments() {
+        List<Department> departments = new ArrayList<>();
+
+        Department department1 = new Department();
+        department1.setId(403);
+        department1.setDepartmentName("IT");
+
+        Department department2 = new Department();
+        department2.setId(770);
+        department2.setDepartmentName("HR");
+
+        departments.add(department1);
+        departments.add(department2);
+
+        return departments;
     }
 
 }
